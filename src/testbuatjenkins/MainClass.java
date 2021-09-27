f73bc1d6a959c3afe20171383097eb3cc08786bf
sadhityapratama <sadhityapratama@gmail.com>
2021-09-27 22:46:32 +0700

    koe lanang loh mas

---
+++ src/testbuatjenkins/MainClass.java
@@ -0,0 +1,11 @@
+package testbuatjenkins;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Hello bitchas \n");
+		System.out.println("Here is your input : " + args[0]);
+	}
+
+}
